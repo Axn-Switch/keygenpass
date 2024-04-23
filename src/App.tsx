b63525daeca5063189394fd1e08e3b786dbadaca
Axn-Switch <dietespifftimothyswift@gmail.com>
2024-04-23 18:52:15 +0100

    second algorithm added

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,35 @@ function App() {
       e.preventDefault()
 
       let pword = "";
-      //first algorithm to generate a password of 10 characters
-      for(var i = 1; i <= 16; i++){
-        pword += String.fromCharCode(Math.random() * (126 - 32) + 32);
-      }
-    
-      setPassword(pword);
+      ;
+
+      //first algorithm
+      //setPassword(pwordtype1(pword).toString());
+
+      setPassword(pwordtype2());
   }
 
+  const pwordtype1 = (pword: String) => {
+    //first algorithm to generate a password of 10 characters
+    for(var i = 1; i <= 16; i++){
+      pword += String.fromCharCode(Math.random() * (126 - 32) + 32);
+    }
+    return pword;
+  }
+
+  const pwordtype2 = () => {
+    let arr = [];
+
+    for(var i = 1; i <= 7; i++){
+      let num: number = Math.floor(Math.random() * (0 - 120) + 120);
+      if(i > 2){
+        arr.push(String.fromCharCode(Math.random() * (126 - 32) + 32));
+      }else
+      arr.push(words.words[num]);
+    }
+
+    return arr.join("")
+  }
 
   return (
     <>
